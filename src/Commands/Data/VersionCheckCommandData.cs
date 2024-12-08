981f230b2ac0cf9814c7a70565eaac952f568700
Lemonical <jesuislemon@gmail.com>
2024-12-08 21:49:59 +0800

    Add client type

--- src/Commands/Data/VersionCheckCommandData.cs
+++ src/Commands/Data/VersionCheckCommandData.cs
@@ -5,6 +5,13 @@ namespace ProjectVTK.Shared.Commands.Data;
 [CommandProtocol(CommandProtocols.VersionCheck)]
 public readonly record struct VersionCheckCommandData : ICommandData
 {
+    public ClientType? Type { get; init; }
     public float? Version { get; init; }
     public string? Message { get; init; }
+}
+
+public enum ClientType
+{
+    Client,
+    Server
 }
